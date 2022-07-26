2ca1105795f8202d79b058e01d17953a114da6f5
Miguel <because_of_peace@yahoo.fr>
2022-07-26 22:45:06 +0200

    Leetcode 185

---
+++ leetcode/KokoEatingBananas.java
@@ -0,0 +1,65 @@
+/**
+ * Lien du problème : https://leetcode.com/problems/koko-eating-bananas/
+ * Youtube Video Lien: 
+ * Auteur de la solution : Miguel Stephane KAKANAKOU
+ * Date: 26-07-2022
+ */
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        long sum = 0;
+        int max = piles[0];
+        
+        for (int tas : piles) {
+            max = Math.max(max, tas);
+            sum += tas;
+        }
+        int min = (int) (sum / h);
+        if (sum%h != 0) {
+            min++;
+        }
+        return searchMin(piles, min, max, h);
+    }
+    
+    private int searchMin(int[] piles, int min, int max, int h) {
+        if (min == max) return max;
+        int mid = (min + max) / 2;
+        boolean isValid = isValidK(piles, mid, h);
+        if (isValid) {
+            return searchMin(piles, min, mid, h);
+        } else {
+            return searchMin(piles, mid+1, max, h);
+        }
+    }
+    
+    private boolean isValidK(int[] piles, int k, int h) {
+        int nbrHours = 0;
+        for (int tas : piles) {
+            int hours = tas / k;
+            if (tas%k != 0) {
+                hours++;
+            }
+            nbrHours += hours;
+        }
+        return nbrHours <= h;
+    }
+}
+
+/*
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+
+
+minimum => 18
+maximum => 30
+
+*/
+
+
+/*
+Input: piles = [3,6,7,11], h = 8
+k = 4
+
+minimum => 27 / 8 => 4
+maximum => 11
+*/
